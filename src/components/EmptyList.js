5bf62a605036741169c217c12e45740e48eb8449
agolikov28 <agolikov@umd.edu>
2023-10-21 00:04:27 -0400

    added empty page animation

--- src/components/EmptyList.js
+++ src/components/EmptyList.js
@@ -3,8 +3,9 @@ import './EmptyList.css'
 
 function EmptyList() {
   return (
-    <div className='emtyList-wraps'>
-        <img src="/assets/images/13525-empty.gif" alt="empty" />
+    <div className='emptyList-wrap'>
+        <img style={{alignSelf: 'center'}} src="/videos copy/page-not-found.gif" alt="page not found" />
+        <h2>Looks like there's nothing here...</h2>
     </div>
   )
 }
